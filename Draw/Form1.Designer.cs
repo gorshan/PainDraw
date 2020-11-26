1232e26fb435f67f68917c471f0a74196a349f73
An <anchka.gor@yandex.ru>
2020-11-26 15:36:17 +0300

    buttons

--- Draw/Form1.Designer.cs
+++ Draw/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.PenButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,11 +46,31 @@
             this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
+            // PenButton
+            // 
+            this.PenButton.Location = new System.Drawing.Point(70, 23);
+            this.PenButton.Name = "PenButton";
+            this.PenButton.Size = new System.Drawing.Size(75, 23);
+            this.PenButton.TabIndex = 1;
+            this.PenButton.Text = "Pen";
+            this.PenButton.UseVisualStyleBackColor = true;
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(189, 23);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(75, 23);
+            this.LineButton.TabIndex = 2;
+            this.LineButton.Text = "Line";
+            this.LineButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LineButton);
+            this.Controls.Add(this.PenButton);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -61,6 +83,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button PenButton;
+        private System.Windows.Forms.Button LineButton;
     }
 }
 
