fe8194f907477bda2c01911d6bca03a84cc59474
Oleg Lyamzin <o.s.like@yandex.ru>
2020-11-28 15:53:02 +0300

    Added AngleFiguresDrawer

---
+++ Draw/Canvases/AngleFiguresDrawer.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Draw.Canvases
+{
+    public class AngleFiguresDrawer : IDrawer
+    {
+        public void DrawFigure(Graphics graphics, Pen pen, Point[] figurePoints)
+        {
+            graphics.DrawPolygon(pen, figurePoints);
+        }
+    }
+}
