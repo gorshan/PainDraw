9ac7baaa6fa0fbaca7d4a3502cbe2652c2fa7d1f
Oleg Lyamzin <o.s.like@yandex.ru>
2020-11-28 15:53:30 +0300

    Added PenDrawer

---
+++ Draw/Canvases/PenDrawer.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace Draw.Canvases
+{
+    internal class PenDrawer : IDrawer
+    {
+        public void DrawFigure(Graphics graphics, Pen pen, Point[] figurePoints)
+        {
+            graphics.DrawCurve(pen, figurePoints);
+        }
+    }
+}
