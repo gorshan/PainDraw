7d575202996f2d1a65de580501c81d11ba317701
An <anchka.gor@yandex.ru>
2020-12-05 00:24:38 +0300

    half-working circle

--- Draw/Figures/CircleFigure.cs
+++ Draw/Figures/CircleFigure.cs
@@ -27,9 +27,11 @@ namespace Draw.Figures
             return Points.ToArray();
         }
 
+
+     
         public void Update(Point startPoint, Point endPoint)
         {
-            int r = (int)Math.Sqrt(Math.Pow(((double)endPoint.Y - startPoint.Y), 2.0) + Math.Pow(((double)endPoint.X - startPoint.X), 2.0));
+            r = (int)Math.Sqrt(Math.Pow(((double)endPoint.Y - startPoint.Y), 2.0) + Math.Pow(((double)endPoint.X - startPoint.X), 2.0));
             int x = startPoint.X - r;
             int y = startPoint.Y - r;
 
@@ -43,32 +45,35 @@ namespace Draw.Figures
         }
 
         public void Move(Point delta)
+        
         {
-            for (int i = 0; i < Points.Count(); i++)
-            {
-                Points[i] = new Point(Points[i].X + delta.X, Points[i].Y + delta.Y);
-            }
+            Points[0] = new Point(Points[0].X + delta.X, Points[0].Y);
+            Points[1] = new Point(Points[1].X + delta.Y, Points[1].Y);
         }
 
         public bool IsThisFigure(Point point)
         {
-            if (Math.Pow((point.X - int(Points[0]) + r), 2) + Math.Pow((point.Y - Points[1] + r), 2) == )
-
-            Point p1 = Points[3];
-            Point p2;
-            foreach (Point p in Points)
+            if (Math.Abs((Math.Pow((point.X - Points[0].X + r), 2) + Math.Pow((point.Y - Points[1].X + r), 2)) - r * r) <= 10)
             {
-                p2 = p;
-                if (Math.Abs((point.X - p1.X) * (p2.Y - p1.Y) - (point.Y - p1.Y) * (p2.X - p1.X))
-                    <= Math.Abs(10 * ((p2.Y - p1.Y) + (p2.X - p1.X)))
-                    && (((p1.X <= point.X) && (point.X <= p2.X)) || ((p1.X >= point.X) && (point.X >= p2.X)))
-                    && (((p1.Y <= point.Y) && (point.Y <= p2.Y)) || ((p1.Y >= point.Y) && (point.Y >= p2.Y))))
-                {
-                    return true;
-                }
-                p1 = p2;
+                return true;
             }
             return false;
+
+            //Point p1 = Points[3];
+            //Point p2;
+            //foreach (Point p in Points)
+            //{
+            //    p2 = p;
+            //    if (Math.Abs((point.X - p1.X) * (p2.Y - p1.Y) - (point.Y - p1.Y) * (p2.X - p1.X))
+            //        <= Math.Abs(10 * ((p2.Y - p1.Y) + (p2.X - p1.X)))
+            //        && (((p1.X <= point.X) && (point.X <= p2.X)) || ((p1.X >= point.X) && (point.X >= p2.X)))
+            //        && (((p1.Y <= point.Y) && (point.Y <= p2.Y)) || ((p1.Y >= point.Y) && (point.Y >= p2.Y))))
+            //    {
+            //        return true;
+            //    }
+            //    p1 = p2;
+            //}
+            //return false;
         }
     }
 }
