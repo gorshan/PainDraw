f3d52c5325b9f7909e27dea97e9eef3df6cb67c9
Oleg Lyamzin <o.s.like@yandex.ru>
2020-12-07 22:32:02 +0300

    Added PipetteMouseHandler

---
+++ Draw/MouseHandlers/PipetteMouseHandler.cs
@@ -0,0 +1,37 @@
+﻿using Draw.Drawer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Draw.MouseHandlers
+{
+    public class PipetteMouseHandler : IMouseHandler
+    {
+        public System.Windows.Forms.Button ColorButton { get; private set; }
+
+        public PipetteMouseHandler(System.Windows.Forms.Button colorButton)
+        {
+            ColorButton = colorButton;
+        }
+
+        public void OnMouseDown(Point location)
+        {
+            Color pixelColor = Canvas.Current.Pen.Color;
+            pixelColor = Canvas.Current.GetImage().GetPixel(location.X, location.Y);
+            ColorButton.BackColor = pixelColor;
+            Canvas.Current.Pen.Color = pixelColor;
+            Canvas.Current.Figure.Color = pixelColor;
+        }
+
+        public void OnMouseMove(Point location)
+        {
+        }
+
+        public void OnMouseUp(Point location)
+        {
+        }
+    }
+}
