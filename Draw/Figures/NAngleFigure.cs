3c8baa62c7f79f215ed0c72e79d210072755d0d0
An <anchka.gor@yandex.ru>
2020-12-09 01:14:33 +0300

    ellipse like nAngle

--- Draw/Figures/NAngleFigure.cs
+++ Draw/Figures/NAngleFigure.cs
@@ -29,11 +29,21 @@ namespace Draw.Figures
         {
             Points = new List<Point>();
             double r;
+            //r = Math.Sqrt(Math.Pow(((double)endPoint.Y - startPoint.Y), 2.0) + Math.Pow(((double)endPoint.X - startPoint.X), 2.0));
             r = Math.Sqrt(Math.Pow(endPoint.Y - startPoint.Y, 2) + Math.Pow(endPoint.X - startPoint.X, 2));
+            double r1;
+            double r2;
+            r1 = (endPoint.X -startPoint.X)/2;
+            r2 = (endPoint.Y - startPoint.Y)/2;
+            double x = startPoint.X + r1;
+            double y = startPoint.Y + r2;
+            
+            
+            
             for (int i = 0; i < N; i++)
             {
-                Points.Add(new Point(Convert.ToInt32(startPoint.X + r * Math.Cos((2 * Math.PI * i) / N)),
-                                      Convert.ToInt32(startPoint.Y + r * Math.Sin((2 * Math.PI * i) / N))));
+                Points.Add(new Point(Convert.ToInt32(x + r1 * Math.Cos((2 * Math.PI * i) / N)),
+                                      Convert.ToInt32(y + r2 * Math.Sin((2 * Math.PI * i) / N))));
             }
         }
 
