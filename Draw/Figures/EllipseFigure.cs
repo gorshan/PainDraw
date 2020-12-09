a3d5fee720300a5c6c452fc728037df15520be98
An <anchka.gor@yandex.ru>
2020-12-09 23:04:28 +0300

    change logic, drawer

--- Draw/Figures/EllipseFigure.cs
+++ Draw/Figures/EllipseFigure.cs
@@ -13,18 +13,33 @@ namespace Draw.Figures
     {
         public EllipseFigure()
         {
-            Drawer = new EllipseDrawer();
+            Drawer = new AngleFiguresDrawer();
         }
 
         public override void Update(PointF startPoint, PointF endPoint)
         {
-            Points = new List<PointF>
+            //Points = new List<PointF>
+            //{
+            //    new PointF(startPoint.X, 0),
+            //    new PointF(startPoint.Y, 0),
+            //    new PointF(endPoint.X - startPoint.X, 0),
+            //    new PointF(endPoint.Y - startPoint.Y, 0)
+            //}; old realization
+            Points = new List<PointF>();
+            int N = 50;
+            double r1;
+            double r2;
+            r1 = (endPoint.X - startPoint.X) / 2;
+            r2 = (endPoint.Y - startPoint.Y) / 2;
+            double x = startPoint.X + r1;
+            double y = startPoint.Y + r2;
+
+            for (int i = 0; i < N; i++)
             {
-                new PointF(startPoint.X, 0),
-                new PointF(startPoint.Y, 0),
-                new PointF(endPoint.X - startPoint.X, 0),
-                new PointF(endPoint.Y - startPoint.Y, 0)
-            };
+                Points.Add(new PointF(Convert.ToInt32(x + r1 * Math.Cos((2 * Math.PI * i) / N)),
+                                     Convert.ToInt32(y + r2 * Math.Sin((2 * Math.PI * i) / N))));
+
+            }
         }
 
         public override void Move(PointF delta)
@@ -82,7 +97,7 @@ namespace Draw.Figures
             }
             else
             {
-                Drawer = new EllipseDrawer();
+                Drawer = new AngleFiguresDrawer();
             }
         }
 
